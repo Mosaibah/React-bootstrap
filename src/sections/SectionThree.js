cb965a619fc275a01f68eafbc8c958ed3bf78ac4
Abdulrahman <abudrm2020@gmail.com>
2021-08-10 14:35:48 +0300

    add function controlled

--- src/sections/SectionThree.js
+++ src/sections/SectionThree.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
 
 export default class SectionThree extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            phone: '',
+            message: ''
+        };
+    
+    }
+    
+    handleInputChange = (event) => {
+        const {value, name} =  event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+        
     render() {
         return (
             <section className="page-section" id="contact">
@@ -13,25 +33,34 @@ export default class SectionThree extends Component {
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-lg-8 col-xl-7">
-                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                        {/* <form id="contactForm" data-sb-form-api-token="API_TOKEN"> */}
                             <div className="form-floating mb-3">
-                                <input className="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+                                <input className="form-control" id="name" name='name' type="text" placeholder="Enter your name..." data-sb-validations="required"
+                                value={this.state.name}
+                                onChange={this.handleInputChange} />
+                                {console.log('Name: ' + this.state.name)}
                                 <label for="name">Full name</label>
                                 <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
                             </div>
                             <div className="form-floating mb-3">
-                                <input className="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                                <input className="form-control" id="email" type="email" name="email" placeholder="name@example.com" data-sb-validations="required,email"
+                                onChange={this.handleInputChange} />
+                                {console.log('Email: ' + this.state.email)}
                                 <label for="email">Email address</label>
                                 <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
                                 <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
                             </div>
                             <div className="form-floating mb-3">
-                                <input className="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
+                                <input className="form-control" id="phone" type="tel" name='phone' placeholder="(123) 456-7890" data-sb-validations="required" 
+                                onChange={this.handleInputChange} />
+                                {console.log('Phone: ' + this.state.phone)}
                                 <label for="phone">Phone number</label>
                                 <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
                             </div>
                             <div className="form-floating mb-3">
-                                <textarea className="form-control" id="message" type="text" placeholder="Enter your message here..." style={{height: "10rem"}} data-sb-validations="required"></textarea>
+                                <textarea className="form-control" id="message" name='message' type="text" placeholder="Enter your message here..." style={{height: "10rem"}} data-sb-validations="required"
+                                onChange={this.handleInputChange} >
+                                </textarea>{console.log('Message: ' + this.state.message)}
                                 <label for="message">Message</label>
                                 <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
                             </div>
@@ -45,7 +74,7 @@ export default class SectionThree extends Component {
                             </div>
                             <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
                             <button className="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Send</button>
-                        </form>
+                        {/* </form> */}
                     </div>
                 </div>
             </div>
