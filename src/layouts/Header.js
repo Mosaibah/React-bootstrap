27f2072c94ec46bb6be4a5a03fc8b616189fd769
Abdulrahman <abudrm2020@gmail.com>
2021-08-12 19:19:13 +0300

    add axios

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 
+import axios from 'axios'
+
 export default class Header extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        };
+    }
+    
+    getPosts = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts', {params: {_limit:5}})
+            .then(res => this.setState({posts: res.data}))
+    }
+
     render() {
+
         return (
             <header className="masthead bg-primary text-white text-center">
             <div className="container d-flex align-items-center flex-column">
@@ -13,6 +29,20 @@ export default class Header extends Component {
                     <div className="divider-custom-line"></div>
                 </div>
                 <p className="masthead-subheading font-weight-light mb-0">Full Stack Web Developer</p>
+
+
+                <button className="btn btn-xl btn-outline-light mt-4" onClick={this.getPosts}>
+                    Load something
+                </button>
+
+                <p>
+                    {this.state.posts.map(posts => 
+                    <h2 key={posts.id} className="mt-5">
+                        Title: {posts.title}
+                    </h2>
+                    )}
+                </p>
+
             </div>
         </header>
         )
